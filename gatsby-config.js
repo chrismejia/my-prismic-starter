c71d13b8e9897f24dbe972b7dccc6926a8c5f5ed
Christian Mejia <chrismejia10@gmail.com>
2020-09-24 17:23:57 -0400

    feat: add Prismic successfully

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Christian Mejia - `,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Christian Mejia`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: "@prismicio/gatsby-source-prismic-graphql",
+      options: {
+        repositoryName: "cm-gatsby-prismic-folio",
       },
     },
     `gatsby-transformer-sharp`,
@@ -27,8 +34,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-purgecss",
+      options: {
+        develop: false, // Activates purging in npm run develop
+        printRejected: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    /**
+     * Automatically generates a _headers file and a _redirects file at the root of the public folder to configure HTTP headers and redirects on Netlify
+     * Make sure to keep this LAST in the plugins array
+     * @see {@link https://www.gatsbyjs.com/plugins/gatsby-plugin-netlify/|gatsby-plugin-netlify}
+     */
+    "gatsby-plugin-netlify",
   ],
 }
